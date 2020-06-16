25e6cb1fc4f9886c4d71816f22d308115a554b75
Dominik Sánchez <4673828+dsancheznet@users.noreply.github.com>
2020-06-16 22:09:54 +0100

    Initial Commit

---
+++ autoclicker/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  autoclicker
+//
+//  Created by Dominik Sánchez Meckenstock on 16/06/2020.
+//  Copyright © 2020 Dominik Sánchez Meckenstock. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
